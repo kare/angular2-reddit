807c8ada839ee4cb9c0d506627c3cb82ad0a108d
Kare Nuorteva <kare.nuorteva@me.com>
2015-10-06 23:42:58 +0300

    Add RedditArticle

--- app.ts
+++ app.ts
@@ -8,9 +8,48 @@ import {
 } from "angular2/angular2";
 
 @Component({
+  selector: 'reddit-article'
+})
+
+@View({
+  template: `
+  <article>
+    <div class="votes">{{ votes }}</div>
+    <div class="main">
+      <h2>
+        <a href="{{ link }}">{{ title }}</a>
+      </h2>
+      <ul>
+        <li><a href (click)='voteUp()'>upvote</a></li>
+        <li><a href (click)='voteDown()'>downvote</a></li>
+      </ul>
+    </div>
+  </article>
+  `
+})
+
+class RedditArticle {
+  votes: number;
+  title: string;
+  link: string;
+  constructor() {
+    this.votes = 10;
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+  }
+  voteUp() {
+    this.votes++
+  }
+  voteDown() {
+    this.votes--
+  }
+}
+
+@Component({
   selector: 'reddit'
-}) @View({
-  directives: [NgFor],
+})
+
+@View({
   template: `
     <section class="new-link">
       <div class="control-group">
@@ -23,7 +62,10 @@ import {
       </div>
       <button (click)="addArticle(newtitle, newlink)">Submit Link</button>
     </section>
-  `
+
+    <reddit-article></reddit-article>
+  `,
+  directives: [RedditArticle]
 })
 
 class RedditApp {
